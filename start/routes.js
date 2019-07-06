64755fdd7e4509a34098d50663a81edc03fd7b42
Alexandre <alebonfinela@gmail.com>
2019-07-06 12:47:11 +0200

    card rentTask-01
    + Add JWT authtentication;
    + Setup pg;
    + create session controller;

--- start/routes.js
+++ start/routes.js
@@ -1,8 +1,10 @@
-'use strict'
+"use strict";
 
-const Route = use('Route');
+const Route = use("Route");
 
-Route.post('/users', 'UserController.create');
-Route.get('/users/:id', 'UserController.display');
-Route.put('/users/:id', 'UserController.update');
-Route.delete('/users/:id', 'UserController.destroy');
+Route.post("/users", "UserController.create");
+Route.get("/users/:id", "UserController.display");
+Route.put("/users/:id", "UserController.update");
+Route.delete("/users/:id", "UserController.destroy");
+
+Route.post("/sessions", "SessionController.create");
